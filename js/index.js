8932b140dddb0e143063de7eef5fb5ee117f820c
Scott Vojik <scott.vojik@gmail.com>
2018-12-12 19:46:11 -0800

    setup page for eventual project demo. used drift video for test

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.onload = function() { //executes code after DOM loads
+  //--- select all <video> on the page
+  const vids = document.querySelectorAll("video")
+  // Loop over the selected elements and add event listeners
+  for (let i = 0; i < vids.length; i++) {
+    vids[i].addEventListener("click", function(e) { 
+      vids[i].play()
+    })
+    vids[i].addEventListener("dblclick", function(e) {
+      vids[i].pause()
+    })
+  }
+ }
