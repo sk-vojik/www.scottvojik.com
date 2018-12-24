dfb1cc0112b6b21d487cacfe86ce12989e277061
Scott Vojik <scott.vojik@gmail.com>
2018-12-24 13:13:29 -0800

    made changes to the video player test

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 //JS Video Player
-window.onload = function() { //executes code after DOM loads
-  //--- select all <video> on the page
-  const vids = document.querySelectorAll("video")
-  // Loop over the selected elements and add event listeners
-  for (let i = 0; i < vids.length; i++) {
-    vids[i].addEventListener("click", function(e) { 
-      vids[i].play()
-    })
-    vids[i].addEventListener("dblclick", function(e) {
-      vids[i].pause()
-    })
-  }
- }
+// window.onload = function() { //executes code after DOM loads
+//   //--- select all <video> on the page
+//   const vids = document.querySelectorAll("video")
+//   // Loop over the selected elements and add event listeners
+//   for (let i = 0; i < vids.length; i++) {
+//     vids[i].addEventListener("click", function(e) { 
+//       vids[i].play()
+//     })
+//     vids[i].addEventListener("dblclick", function(e) {
+//       vids[i].pause()
+//     })
+//   }
+//  }
 
 
  //Scroll Reveal
